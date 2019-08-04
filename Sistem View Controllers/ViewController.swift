980227db4bd9095be19731cfd1c1db61db7f51c9
JohnKostanov <kostanovd@icloud.com>
2019-08-04 16:33:24 +0200

    Initial commit

--- Sistem View Controllers/ViewController.swift
+++ Sistem View Controllers/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBAction func shareButtonPressed(_ sender: UIButton ) {
+        print(#line, #function)
     }
-
-
+    
+    @IBAction func safariButtonPressed(_ sender: UIButton ) {
+        print(#line, #function)
+    }
+    
+    @IBAction func cameraButtonPressed(_ sender: UIButton ) {
+        print(#line, #function)
+    }
+    
+    @IBAction func emailButtonPressed(_ sender: UIButton ) {
+        print(#line, #function)
+    }
 }
 
