961bf524119d7d718d4b9e52755945ca1c2a92ac
JohnKostanov <kostanovd@icloud.com>
2019-08-04 22:44:35 +0200

    Add function updateUI

--- Sistem View Controllers/ViewController.swift
+++ Sistem View Controllers/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARH:- Outlets
+    @IBOutlet var stackView: UIStackView!
+    @IBOutlet var imageView: UIImageView!
+    
+    // MARK: - UIViewController Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI(with: view.bounds.size)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        updateUI(with: size)
+        
+    }
+    
+    // MARK: - UI Methods
+    func updateUI(with size: CGSize) {
+        let isVertical = size.width < size.height
+        stackView.axis = isVertical ? .vertical : .horizontal
+    }
+    
+    // MARK: - Actions
     @IBAction func shareButtonPressed(_ sender: UIButton ) {
         print(#line, #function)
     }
